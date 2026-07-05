410a0bd287d5c028bbcd66385d11a11ada632d69
Zahotoo <Zjh050330@gmail.com>
2026-07-05 04:07:49 +0100

    feat: add BaseEntity (@MappedSuperclass) and JPA auditing config

---
+++ backend/src/main/java/com/unitrovee/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.unitrovee.common;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    // Primary key defined
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // the primary key will be generated automatically
+    private Long id;
+
+    // Insert the current time when created
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
