e090eeb88ae827e1449460ba8bd9edb3e4491b59
Jiahao Zhu <146834485+Zahotoo@users.noreply.github.com>
2026-07-05 04:11:19 +0100

    feat: add BaseEntity (@MappedSuperclass) and JPA auditing config (#23)

---
+++ backend/src/main/java/com/unitrovee/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.unitrovee.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
