e3411fddce431fc2078bca2e67e0220c03679378
Jiahao Zhu <146834485+Zahotoo@users.noreply.github.com>
2026-07-25 00:51:11 +0100

    add item delete and archive workflow (#71)

--- backend/src/main/java/com/unitrovee/item/ItemService.java
+++ backend/src/main/java/com/unitrovee/item/ItemService.java
@@ -9,4 +9,6 @@ public interface ItemService {
     ItemCreateResponse createItem(String authenticatedEmail, ItemCreateRequest request);
 
     void updateItem(Long itemId, String authenticatedEmail, ItemUpdateRequest request);
+
+    void deleteItem(Long itemId, String authenticatedEmail);
 }
